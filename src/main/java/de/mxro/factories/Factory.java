6cde99483c16a8dbbba54f0ca5547eb0fc4a7efe
mxro <mxro@nowhere.com>
2014-08-11 13:52:54 +1000

    Improving persistence API Autocommit Mon Aug 11 13:52:54 EST 2014

--- src/main/java/de/mxro/factories/Factory.java
+++ src/main/java/de/mxro/factories/Factory.java
@@ -1,13 +1,26 @@
 package de.mxro.factories;
 
 /**
- * A factory which can instantate objects from a {@link Configuration} and run-time {@link Dependencies}.
+ * <p>
+ * A factory which can instantate objects from a {@link Configuration} and
+ * run-time {@link Dependencies}.
+ * 
  * 
  * @author Max
  * 
  */
-public interface Factory<ObjectType, ConfigurationType extends Configuration,DependenciesType extends Dependencies> {
+public interface Factory<ObjectType, ConfigurationType extends Configuration, DependenciesType extends Dependencies> {
 
+	/**
+	 * <p>
+	 * This method tests whether the supplied {@link Configuration} can be
+	 * instantiated into an object by this factory.
+	 * 
+	 * @param conf
+	 *            A configuration for an object to be instantated.
+	 * @return <code>true</code> when this factory can create an instance for
+	 *         the provided configuration.
+	 */
 	public boolean canInstantiate(Configuration conf);
 
 	public ObjectType create(ConfigurationType conf,
