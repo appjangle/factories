fb5538661eec5c2d1b43261b2cd24d70bdf7698b
mxro <mxro@nowhere.com>
2014-09-18 09:15:32 +1000

    Reworking embedded server API Autocommit Thu Sep 18 09:15:32 EST 2014

--- src/main/java/de/mxro/factories/Factories.java
+++ src/main/java/de/mxro/factories/Factories.java
@@ -3,21 +3,26 @@ package de.mxro.factories;
 import de.mxro.factories.internal.FactoryCollectionImpl;
 
 /**
- * Entry point for factories module. Allows to create new {@link FactoryCollection}s.
+ * Entry point for factories module. Allows to create new
+ * {@link FactoryCollection}s.
+ * 
  * @author Max
  *
  */
 public class Factories {
 
-	/**
-	 * Creates a new collection of factories.
-	 * 
-	 * @return A new factory collection.
-	 */
-	public static FactoryCollection create() {
-		return new FactoryCollectionImpl();
-	}
+    /**
+     * Creates a new collection of factories.
+     * 
+     * @return A new factory collection.
+     */
+    public static FactoryCollection create() {
+        return new FactoryCollectionImpl();
+    }
 
-	
-	
+    public static Dependencies noDependencies() {
+        return new Dependencies() {
+        };
+    }
+
 }
